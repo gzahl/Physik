7a4f43a686134e0be37d10bd27948ca4f048b5ef
Manuel Jung <gzahl@arcor.de>
2010-10-27 21:58:22 +0200

    Update

--- update.sh
+++ update.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 cd 1-Fach
-wget --output-document="Prüfungsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/6.3-121.pdf
-wget --output-document="Prüfungsverfahrensordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-2.1.pdf
-wget --output-document="Praktikumsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-9.2.pdf
-pdftotext *.pdf
+wget --output-document="Prüfungsordnung.pdf"  http://www.studservice.uni-kiel.de/sta/6.3-121.pdf
+wget --output-document="Prüfungsverfahrensordnung.pdf"  http://www.studservice.uni-kiel.de/sta/0-1-2.1.pdf
+wget --output-document="Praktikumsordnung.pdf"  http://www.studservice.uni-kiel.de/sta/0-1-9.2.pdf
+for pdffile in *.pdf
+do
+pdftotext $pdffile
+done
 cd ..
 cd 2-Fach
-wget --output-document="Prüfungsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/6.3-122.pdf
-wget --output-document="2-Fach-Prüfungsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-2.2.pdf
-wget --output-document="Prüfungsverfahrensordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-2.1.pdf
-wget --output-document="Praktikumsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-9.pdf
-pdftotext *.pdf
+wget --output-document="Prüfungsordnung.pdf"  http://www.studservice.uni-kiel.de/sta/6.3-122.pdf
+wget --output-document="2-Fach-Prüfungsordnung.pdf"  http://www.studservice.uni-kiel.de/sta/0-1-2.2.pdf
+wget --output-document="Prüfungsverfahrensordnung.pdf"  http://www.studservice.uni-kiel.de/sta/0-1-2.1.pdf
+wget --output-document="Praktikumsordnung.pdf"  http://www.studservice.uni-kiel.de/sta/0-1-9.pdf
+
+for pdffile in *.pdf
+do
+pdftotext $pdffile
+done
+
 cd ..
 git add .
 git commit -a -m"Update"
--- update.sh~
+++ update.sh~
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd 1-Fach
-wget --Output="Prüfungsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/6.3-121.pdf
-wget --Output="Prüfungsverfahrensordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-2.1.pdf
-wget --Output="Praktikumsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-9.2.pdf
+wget --output-document="Prüfungsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/6.3-121.pdf
+wget --output-document="Prüfungsverfahrensordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-2.1.pdf
+wget --output-document="Praktikumsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-9.2.pdf
 pdftotext *.pdf
 cd ..
 cd 2-Fach
-wget --Output="Prüfungsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/6.3-122.pdf
-wget --Output="2-Fach-Prüfungsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-2.2.pdf
-wget --Output="Prüfungsverfahrensordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-2.1.pdf
-wget --Output="Praktikumsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-9.pdf
+wget --output-document="Prüfungsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/6.3-122.pdf
+wget --output-document="2-Fach-Prüfungsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-2.2.pdf
+wget --output-document="Prüfungsverfahrensordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-2.1.pdf
+wget --output-document="Praktikumsordnung.pdf" -N http://www.studservice.uni-kiel.de/sta/0-1-9.pdf
 pdftotext *.pdf
 cd ..
 git add .
